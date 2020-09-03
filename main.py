7b5a0f503a745db98bb68c7c85580602ce650711
Damian Sztankowski <damian.sztankowski@gmail.com>
2020-09-03 14:18:41 +0200

    first draft of python GUI APP

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
 
-print ('Hello, this is my first python script')
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
 
-print ('second chance ;)')
+
+root = tk.Tk()
+
+
+canvas = tk.Canvas(root, height=700, width=700,bg="#263D48")
+canvas.pack()
 
 
 
-dsdsdsd
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+
+openFile = tk.Button(root, text="Open file", padx=10, pady=5, fg="darkgrey", bg="grey")
+openFile.pack()
+
+runApp = tk.Button(root, text="Run App", padx=10, pady=5, fg="darkgrey", bg="grey")
+runApp.pack()
+
+root.mainloop()
+
+
+
